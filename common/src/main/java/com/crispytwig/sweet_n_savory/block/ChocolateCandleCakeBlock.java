dbfbec134052366abea269ed1ce0f68ddedde8c1
crispytwig <lukehartinger@outlook.com>
2022-11-30 18:50:34 -0600

    [~+] Fixed Chocolate Candle Cakes by adding loot tables. Who'd've thought? :')

--- common/src/main/java/com/crispytwig/sweet_n_savory/block/ChocolateCandleCakeBlock.java
+++ common/src/main/java/com/crispytwig/sweet_n_savory/block/ChocolateCandleCakeBlock.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package com.crispytwig.sweet_n_savory.block;
 
 import com.crispytwig.sweet_n_savory.registry.ModBlocks;
